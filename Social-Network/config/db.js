5414db526e1236f2f7c407486b70c3cdf0dc6689
George <angelov2000@abv.bg>
2021-03-25 19:19:58 +0200

    back end resources

---
+++ Social-Network/config/db.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+const config=require('config');
+const db=config.get('MONGODB_URL');
+
+const connectDB= async ()=>{
+    try {
+       await mongoose.connect(db,{
+           useNewUrlParser:true,
+           useUnifiedTopology: true
+       });
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports=connectDB;
