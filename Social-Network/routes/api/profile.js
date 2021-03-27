c6e7836ad619f68b660568cf903a158c7dd3c18d
George <angelov2000@abv.bg>
2021-03-27 08:32:58 +0200

    Setting up profile model

--- Social-Network/routes/api/profile.js
+++ Social-Network/routes/api/profile.js
@@ -1,12 +1,28 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
 
-//@route GET api/profile
-//@description Test route
-//@Access Public
+const Profile = require("../../models/profile");
+const User = require("../../models/user");
+//@route GET api/profile/me
+//@description Get current users profiles
+//@Access Private
 
-router.get('/',(req,res)=>{
-    res.send('Profile route')
+router.get("/me",auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There is not profile for this user" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
-module.exports=router;
+module.exports = router;
