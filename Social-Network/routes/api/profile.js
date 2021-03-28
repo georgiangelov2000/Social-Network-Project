5a7d7503c8d77b37fd408038705d6390ff518a14
George <angelov2000@abv.bg>
2021-03-28 10:19:22 +0300

    Completed profile post data

--- Social-Network/routes/api/profile.js
+++ Social-Network/routes/api/profile.js
@@ -22,7 +22,8 @@ router.get("/me", auth, async (req, res) => {
   }
 });
 
-router.post("/",
+router.post(
+  "/",
   [
     auth,
     [
@@ -31,11 +32,11 @@ router.post("/",
     ],
   ],
   async (req, res) => {
-    const errors=validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.status(400).json({errors:errors.array() });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    const{
+    const {
       company,
       website,
       location,
@@ -48,29 +49,46 @@ router.post("/",
       facebook,
       linkedin,
       instagram,
-    }=req.body;
+    } = req.body;
     //Build profile project
-    const profileFields={};
-    profileFields.user=req.user.id;
-    if(company) profileFields.company=company;
-    if(website) profileFields.website=website;
-    if(location) profileFields.location=location;
-    if(bio) profileFields.bio=bio;
-    if(status) profileFields.status=status;
-    if(githubusername) profileFields.githubusername=githubusername;
-    if(skills){
-      profileFields.skills=skills.split(',').map(skill=>skill.trim());
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
     }
     //Build social object
-    profileFields.social={};
-    if(youtube) profileFields.social.youtube=youtube;
-    if(twitter) profileFields.social.twitter=twitter;
-    if(facebook) profileFields.social.facebook=facebook;
-    if(linkedin) profileFields.social.linkedin=linkedin;
-    if(instagram) profileFields.social.instagram=instagram
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
 
-    console.log(profileFields.skills);
-    res.send('Hello');
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        //update profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      //Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile); 
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
