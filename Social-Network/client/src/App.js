77ce71deca9bc589aa0e1ca91e2edee5d3531188
George <angelov2000@abv.bg>
2021-03-30 19:30:11 +0300

    Setting up react router

--- Social-Network/client/src/App.js
+++ Social-Network/client/src/App.js
@@ -1,15 +1,23 @@
-import React, {Fragment} from 'react'
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import Navbar from './components/Layout/Navbar';
-import Landing from './components/Layout/Landing';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar";
+import Landing from "./components/Layout/Landing";
 
-const App=()=>(
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import "./App.css";
+
+const App = () => (
   <Router>
     <Fragment>
       <Navbar />
-      <Router exact path='/' component={Landing} />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/Login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 );
