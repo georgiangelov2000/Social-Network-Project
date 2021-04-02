5dc112a0c7860321ece52ab58975fa880e893385
George <angelov2000@abv.bg>
2021-04-02 14:43:15 +0300

    settinh up auth components

--- Social-Network/client/src/components/Auth/Register.js
+++ Social-Network/client/src/components/Auth/Register.js
@@ -2,10 +2,10 @@ import React, { Fragment, useState } from "react";
 import {connect} from "react-redux";
 import {setAlert} from "../../actions/alerts";
 import {register} from "../../actions/auth";
-import {Link} from 'react-router-dom';
+import {Link,Redirect} from 'react-router-dom';
 import PropTypes from "prop-types";
 
-export const Register = ({setAlert,register}) => {
+export const Register = ({setAlert,register,isAuthenticated}) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -24,6 +24,10 @@ export const Register = ({setAlert,register}) => {
      register({name,email,password});
     }
   };
+
+  if(isAuthenticated){
+    return <Redirect to="/dashboard" />
+  }
 
   return (
     <Fragment>
@@ -81,10 +85,15 @@ export const Register = ({setAlert,register}) => {
 
 Register.propTypes = {
   setAlert:PropTypes.func.isRequired,
-  register:PropTypes.func.isRequired
+  register:PropTypes.func.isRequired,
+  isAuthenticated:PropTypes.bool,
 };
 
+const mapStateToProps = state=>({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
 export default connect(
-  null,
+  mapStateToProps,
   { setAlert,register }
 ) (Register);
